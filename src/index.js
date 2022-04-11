c2d82581a7fbb9fab4d58ff487cf387b8e256991
Ryo-Ura <urawaryosuke@gmail.com>
2022-04-11 14:09:44 -0700

    changed env variable name and fixed bugs

--- src/index.js
+++ src/index.js
@@ -11,25 +11,24 @@ const app = express()
 
 const SYNC_OUT_DIR = process.env.SYNC_OUT_DIR;
 const SYNC_IN_DIR = process.env.SYNC_IN_DIR;
-watch(SYNC_OUT_DIR, {recursive: false}, async(evt, name)=>{
+watch(SYNC_IN_DIR, {recursive: false}, async(evt, name)=>{
     // console.log("updated: ", name);
     const file = fs.createReadStream(name);
     
     const form = new FormData();
     form.append('file', file);
     
-    const response = await fetch(`${precess.env.REMOTE_URL}files`,{
+    const response = await fetch(`${process.env.REMOTE_URL}files`,{
       method: 'POST', 
       body: form,
     });
-    fs.unlinkSync(name);
 });
 
 
 app.post('/files', upload.single("file"), function (req, res, next) {
-  const file = fs.readFileSync(path);
-  fs.writeFileSync(nodePath.join(SYNC_IN_DIR, originalname), file);
-  fs.unlinkSync(path);
+  const file = fs.readFileSync(nodePath.join(SYNC_IN_DIR, req.file.originalname));
+  fs.writeFileSync(nodePath.join(SYNC_OUT_DIR, req.file.originalname), file);
+  fs.unlinkSync(nodePath.join(SYNC_IN_DIR, req.file.originalname));
   res.status(200);
   res.send("success");
 });
